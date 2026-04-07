c16d8484183bff17790b433c23451f22602a0840
Shobhit ☮️ <141612496+shobhit164@users.noreply.github.com>
2026-04-07 01:18:09 -0400

    Update run.py

--- run.py
+++ run.py
@@ -4,5 +4,5 @@ app = create_app()
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000) #check the port connection
+    app.run(host="0.0.0.0", port=5000) #check the port connection -> Thanks for the heads up charan! I checked eveything it is working fine! The problem with port number is resolved now!
 
